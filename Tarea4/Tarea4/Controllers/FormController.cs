bc51ac6ecb0af371dea33010bbfd0dc48f2128b5
mishyarias0410@gmail.com <mishyarias0410@gmail.com>
2020-02-28 10:04:55 -0400

    Se crearon los controladores y las vistas necesarias para el formulario de solicitud de empleo, las cuales tambien permiten al usuario introducir una foro y cargar su curriculum

---
+++ Tarea4/Tarea4/Controllers/FormController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tarea4.Models;
+
+namespace Tarea4.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Resultados(Datos datos, HttpPostedFileBase pic, HttpPostedFileBase curriculum)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string nombreFoto = pic.FileName;
+                pic.SaveAs(Server.MapPath("/image/ " + nombreFoto));
+                ViewBag.Picture = nombreFoto;
+
+                string nombreCurriculum = curriculum.FileName;
+                curriculum.SaveAs(Server.MapPath("/pdf/" + nombreCurriculum));
+                ViewBag.curriculum = nombreCurriculum;
+                return View(datos);
+
+            }
+
+
+            else
+            {
+                return View("Formulario");
+            }
+        }
+    }
+}
---
+++ Tarea4/Tarea4/Models/Datos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tarea4.Models
+{
+    public class Datos
+    {
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Cedula { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Apellido { get; set; }
+        [Range(15, 90, ErrorMessage = "Debes ser mayor de 15 años")]
+        public int Edad { get; set; }
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Formato incorrecto")]
+        public string Email { get; set; }
+        public genero Genero { get; set; }
+        public puesto Puesto { get; set; }
+
+    }
+
+    public enum genero
+    {
+        Masculino, Femenino
+    }
+    public enum puesto
+    {
+        Desarrollador, Analista, Diseñador, Secretario
+    }
+}
