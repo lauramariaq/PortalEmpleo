3eb8165c15f876fd652e414db0f9d36e0b22c252
LauraMaria <lauraqh2009@hotmail.com>
2020-02-28 09:03:57 -0400

    se creo la vista para la conversion de medidas

---
+++ Tarea4/Tarea4/Controllers/conversionesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea4.Controllers
+{
+    public class conversionesController : Controller
+    {
+        // GET: conversiones
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult conversion()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tarea4/Tarea4/Models/conversion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tarea4.Models
+{
+    public class conversion
+    {
+    }
+}
